d813b157b9f5b907161908bc5fbf98f46abef3a5
Alexander Vynnyk <oleksandr.vynnyk@thomascookonline.com>
2017-02-07 18:02:52 +0200

    Add license

--- src/wrappers/jab/jabs-argparse.lisp
+++ src/wrappers/jab/jabs-argparse.lisp
@@ -1,17 +1,27 @@
-;; ;;; The following lines added by ql:add-to-init-file:
-;; #-quicklisp
-;; (let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
-;;                                        (user-homedir-pathname))))
-;;   (when (probe-file quicklisp-init)
-;;     (load quicklisp-init)))
+;;; -*- Mode: Lisp -*-
+#|
+MIT License
 
-;; (ql:quickload "log4cl")
-;; parent parser - parser, which goes after
+Copyright (c) 2017 Alexander Vynnyk <cosmonaut.ok@zoho.com>
 
-;; (defvar *args* (make-hash-table :test 'equal))
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; COMMON ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+|#
 (in-package :tools@jabs)
 
 (export '(make-argument-parser
